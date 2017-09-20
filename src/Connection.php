6d91396c67bfdbd60e50dab209b938f20c4da30a
micrarior <log.kot@gmail.com>
2017-09-20 09:36:30 +0500

    Convert mailbox encoding to utf-8

--- src/Connection.php
+++ src/Connection.php
@@ -75,7 +75,7 @@ class Connection
             throw new MailboxDoesNotExistException($name);
         }
 
-        return new Mailbox($this->server . imap_utf7_encode($name), $this);
+        return new Mailbox($this->server . mb_convert_encoding($name, 'utf7-imap', 'utf-8'), $this);
     }
 
     /**
@@ -151,7 +151,7 @@ class Connection
         if (null === $this->mailboxNames) {
             $mailboxes = imap_getmailboxes($this->resource, $this->server, '*');
             foreach ($mailboxes as $mailbox) {
-                $this->mailboxNames[] = imap_utf7_decode(str_replace($this->server, '', $mailbox->name));
+                $this->mailboxNames[] = mb_convert_encoding(str_replace($this->server, '', $mailbox->name), 'utf-8', 'utf7-imap');
             }
         }
 
